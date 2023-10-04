07cbc03db7e51b15b49874e1436044b7bd73234b
axelca <cuevas.axel@uabc.edu.mx>
2023-10-03 18:01:37 -0700

    practicas restantes y ejercicio24= primer ejercicio js

---
+++ PRACTICA24/ejemplo.js
@@ -0,0 +1,31 @@
+/*
+var numero1 = prompt("ingrese numero 1");
+var numero2 = prompt("ingrese numero 2");
+var resultado = (numero1 + numero2);
+
+alert(resultado);
+*/
+/*
+var numero1 = "bienbenidos a nuestro sitio";
+var numero2 = "texto comillas dobles";
+var resultado = 'texto comillas simples';
+
+alert(resultado+numero1+numero2);
+*/
+
+/*
+var clienteregistro=false;
+var ivaincluido=true;
+alert(clienteregistro)
+*/
+/*
+var nombre_array= ['tijuana','rosarito'];
+alert(nombre_array);
+*/
+var numero1 = prompt("ingrese numero 1");
+var numero2 = prompt("ingrese numero 2");
+var SUMA = parseInt(numero1) + parseInt(numero2);
+var resta = parseInt(numero1) - parseInt(numero2);
+var multi = parseInt(numero1) * parseInt(numero2);
+var div = parseInt(numero1) / parseInt(numero2);
+alert("numero 1 = "+numero1+" numero 2 = "+numero2+ "\n suma= "+SUMA+ " resta= "+resta+" multiplicacion= "+multi +" divicion= "+div);
