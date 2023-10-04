85073c567d9a8cc65211105d18b68324df52a3e1
axelca <cuevas.axel@uabc.edu.mx>
2023-10-03 18:32:52 -0700

    realizamos ejercicios con los operadores

---
+++ PRACTICA25/p25.js
@@ -0,0 +1,9 @@
+var valores =[true,10,false,"codigo","herramientas",7];
+var resultado= valores[3] < valores[4];
+var SUMA= valores[1] + valores[5];
+var resta= valores[1] - valores[5];
+var multi= valores[1] * valores[5];
+var div= valores[1] / valores[5];
+var pepe= valores[0] != valores[2];
+ 
+alert(resultado +  "\n"+pepe+"\n suma= "+SUMA+ " resta= "+resta+" multiplicacion= "+multi +" divicion= "+div);
