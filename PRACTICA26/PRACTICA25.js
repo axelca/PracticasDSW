6629cdbd1cef8553b91fcf5da09924f11e39a8a8
axelca <cuevas.axel@uabc.edu.mx>
2023-10-09 19:49:11 -0700

    PRIMEROS EJERCICIOS DE SICLOS IF Y IF ELSE

---
+++ PRACTICA26/PRACTICA25.js
@@ -0,0 +1,12 @@
+var num1=5
+var num2=8
+
+if( num1 <= num2  ){
+    alert("num 1 no es mayor que num 2")
+}
+if( num2 <= 0  ){
+    alert("num2 es positivo")
+}
+if(++num1 < num2){
+    alert("incrementar uno en numero 1, no lo hace mayor a numero 2")
+}
