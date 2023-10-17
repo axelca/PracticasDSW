73bbded9b1f27dce35b3f485296ac058daee4b1a
axelca <cuevas.axel@uabc.edu.mx>
2023-10-16 19:53:57 -0700

    trabajo de funciones donde aplicamos diversas funciones

---
+++ PRACTICA30/p30.js
@@ -0,0 +1,60 @@
+//ejercicio 2
+
+var num = 2;
+
+function par_y_impar(){
+    if (num % 2 == 0) {
+    return "Es un número par";
+  } else {
+    return "Es un número impar";
+  }
+
+}
+alert(par_y_impar())
+
+//ejercicio 2
+/*
+function avisoDeHuracan(hayUnHuracan) {
+    if (hayUnHuracan) {
+      alert("Hay un Huracan");
+    } else {
+      alert("Esta Soleado");
+    }
+  }
+ 
+  var hayUnHuracanHoy = true;
+  avisoDeHuracan(hayUnHuracanHoy); 
+  
+  var hayUnHuracanMañana = false;
+  avisoDeHuracan(hayUnHuracanMañana);
+  */
+
+  // ejercicio3
+  /*
+var operacion="sumar";
+var a=3;
+var b=8;
+  function OperacionesMatematicas() {
+    switch (operacion) {
+      case "sumar":
+        return a + b;
+      case "restar":
+        return a - b;
+      case "multiplicar":
+        return a * b;
+      case "dividir":
+        if (b !== 0) {
+          return a / b;
+        } else {
+          return "No se puede dividir por cero";
+        }
+      default:
+        return "0";
+    }
+  }
+  alert(OperacionesMatematicas());
+ */
+  
+//axel cuevas
+
+
